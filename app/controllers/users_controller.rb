a6e6b9dc8c8e6d8371f62dc3e170d8c2993da27f
yuliatokaryk <julia4099@gmail.com>
2022-07-18 10:15:03 +0300

    added edit template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,34 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the AlphaBlog, #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = Usef.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "your accout information was updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
