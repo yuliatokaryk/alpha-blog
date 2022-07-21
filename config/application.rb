15a29b96e52bf5a7792aa47ba52dbd37847b8e3b
yuliatokaryk <julia4099@gmail.com>
2022-07-21 13:10:44 +0300

    locales added

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module AlphaBlog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :uk]
+    config.i18n.default_locale = :uk
   end
 end
