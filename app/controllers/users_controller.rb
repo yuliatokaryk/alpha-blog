552f67f03dbedf9035df951bfa15c945fe2ee7dc
yuliatokaryk <julia4099@gmail.com>
2022-07-21 16:47:35 +0300

    about added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :about)
   end
 
   def set_user
