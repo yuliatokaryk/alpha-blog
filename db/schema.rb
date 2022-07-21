c365a42510a6d31dfdcbefd59c42cf1ee05a5439
yuliatokaryk <julia4099@gmail.com>
2022-07-21 16:28:53 +0300

    added about to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_21_123856) do
+ActiveRecord::Schema.define(version: 2022_07_21_132755) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_07_21_123856) do
     t.string "password_digest"
     t.boolean "admin", default: false
     t.decimal "number"
+    t.text "about"
   end
 
 end
