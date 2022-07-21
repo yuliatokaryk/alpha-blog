23cbde59da82ed10202394c50e1356855b064a36
yuliatokaryk <julia4099@gmail.com>
2022-07-21 15:08:36 +0300

    fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:notice] = t("welcome_to")", #{@user.username}"
+      flash[:notice] = t("welcome_to")
       redirect_to articles_path
     else
       render 'new'
