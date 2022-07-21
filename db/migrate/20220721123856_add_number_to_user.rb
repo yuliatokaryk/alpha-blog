73041eb1e7d142ccced2f573e35d4e8fc23edd5b
yuliatokaryk <julia4099@gmail.com>
2022-07-21 18:40:51 +0300

    number fixed

--- db/migrate/20220721123856_add_number_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :number, :number
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_21_132755) do
+ActiveRecord::Schema.define(version: 2022_07_21_150832) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2022_07_21_132755) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.boolean "admin", default: false
-    t.decimal "number"
     t.text "about"
   end
 
