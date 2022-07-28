8f49bbbfa25f745e48405e86e71e0a873154a17f
yuliatokaryk <julia4099@gmail.com>
2022-07-28 11:44:35 +0300

    has many association added

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 30, maximum: 10000 }
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
